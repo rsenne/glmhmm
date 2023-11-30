4993c66494693a54cc8ca7502d46ae9457116c9e
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2023-11-30 17:59:04 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='glmhmm',
+    version='0.0.0',
+    packages=['glmhmm'],
+    url='',
+    license='',
+    author='irisstone',
+    author_email='',
+    description=''
+)
